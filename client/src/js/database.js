4aa410f6b51c1ea78de6762fd335cedea0bc48c5
smithm4949 <smithm4949@gmail.com>
2023-04-03 20:53:12 -0400

    adding functionality for getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,21 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('reading from idb');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
